c09dd1110d84fc53d2400c35992e72b4f28db0ca
Jay Patel <55126933+jaipatel248@users.noreply.github.com>
2024-03-26 04:28:17 +0000

    Added BottomBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BottomBar from "./components/bottomBar/BottomBar";
+import { Container } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      style={{
+        padding: 0,
+        height: "100vh",
+        boxShadow: "2px 2px 4px rgba(100, 0, 0, 0.1)",
+        border: "1px solid red",
+      }}
+      maxWidth={false}
+    >
+      <BottomBar />
+    </Container>
   );
 }
 
---
+++ src/components/bottomBar/BottomBar.tsx
@@ -0,0 +1,65 @@
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  BottomNavigationActionProps,
+  Paper,
+} from "@mui/material";
+import React, { useState } from "react";
+import MarkChatUnreadIcon from "@mui/icons-material/MarkChatUnread";
+import MarkChatUnreadOutlinedIcon from "@mui/icons-material/MarkChatUnreadOutlined";
+import PhoneIcon from "@mui/icons-material/Phone";
+import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
+import GroupsIcon from "@mui/icons-material/Groups";
+import GroupsOutlinedIcon from "@mui/icons-material/GroupsOutlined";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import QuestionAnswerOutlinedIcon from "@mui/icons-material/QuestionAnswerOutlined";
+import SettingsIcon from "@mui/icons-material/Settings";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+const CustomBottomNavigationAction = (props: BottomNavigationActionProps) => {
+  return <BottomNavigationAction {...props}></BottomNavigationAction>;
+};
+
+function BottomBar() {
+  const [selectedItem, setSelectedItem] = useState(2);
+
+  return (
+    <Paper
+      sx={{
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}
+      elevation={3}
+    >
+      <BottomNavigation
+        showLabels
+        value={selectedItem}
+        onChange={(event, newValue) => {
+          setSelectedItem(newValue);
+        }}
+      >
+        <CustomBottomNavigationAction
+          value
+          label="Updates"
+          icon={<MarkChatUnreadIcon />}
+        />
+        <CustomBottomNavigationAction label="Calls" icon={<PhoneIcon />} />
+        <CustomBottomNavigationAction
+          label="Communities"
+          icon={<GroupsIcon />}
+        />
+        <CustomBottomNavigationAction
+          label="Chats"
+          icon={<QuestionAnswerIcon />}
+        />
+        <CustomBottomNavigationAction
+          label="Settings"
+          icon={<SettingsIcon />}
+        />
+      </BottomNavigation>
+    </Paper>
+  );
+}
+
+export default BottomBar;
