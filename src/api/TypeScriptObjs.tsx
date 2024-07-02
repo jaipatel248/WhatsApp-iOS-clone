a061c562c5fcca2d834158d3659862cdc45453b1
Jay Patel <55126933+jaipatel248@users.noreply.github.com>
2024-07-02 12:33:45 +0000

    Pending changes exported from your codespace

--- src/api/TypeScriptObjs.tsx
+++
@@ -1,7 +0,0 @@
-export type IChatObject = {
-  id: number;
-  name: string;
-  last_chat_date: string; // You might want to use a Date type here if you plan to parse the date string
-  unread_message_count: number;
-  last_message: string;
-};
